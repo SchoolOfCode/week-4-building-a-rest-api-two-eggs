bd06dd6ea6f908693e343c48df8dbc11677a5f93
Louis <178508612+L-Brookling@users.noreply.github.com>
2024-12-17 14:35:56 +0000

    Complete ticket 3

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.get("/", function (req, res) {
   res.send("Welcome to the inspirational quotes API");
 });
 
+app.get('/quotes', async function (req, res) {
+  res.json(await getQuotes())
+});
+// Write a request handler
+// Use get request
+// It returns object with all quotes
+// Use a helper function from quotes.js
+
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
